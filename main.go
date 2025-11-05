d85d9eb38cb277a4d2fb4d8b0dd67f54fe29666c
Adam, darkrimson <arsanov2003@yandex.ru>
2025-11-05 21:34:46 +0300

    sql bug

--- main.go
+++ main.go
@@ -98,8 +98,9 @@ func (s ParcelService) Delete(number int) error {
 
 func main() {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "tracker.db")
 
-	store := // создайте объект ParcelStore функцией NewParcelStore
+	store := NewParcelStore(db) // создайте объект ParcelStore функцией NewParcelStore
 	service := NewParcelService(store)
 
 	// регистрация посылки
